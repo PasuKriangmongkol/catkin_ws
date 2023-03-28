f5c5640a29bc371ff84db206c0154b17c2de94ce
PasuKriangmongkol <dominicsz2012@gmail.com>
2023-03-28 21:16:06 +0700

    change

--- src/my_robot_controller/script/my_robot.py
+++ src/my_robot_controller/script/my_robot.py
@@ -5,23 +5,28 @@ import smach_ros
 from std_srvs.srv import *
 from your_smach.srv import GoToPosition, GoToPositionRequest
 
-class DoSth(smach.State):
+class IDLE(smach.state) :
+    def __init__(self) -> None:
+        super().__init__(self,outcomes=['success', 'fail']):
+        if 
+
+class H2G(smach.State):
     def __init__(self, outcomes=['success', 'fail']):
         super().__init__(outcomes)
         self.dosth_ser = rospy.ServiceProxy('robot/do_sth', Empty)
     
     def execute(self, ud):
-        rospy.loginfo("Executing state DoSth")
+        rospy.loginfo("Executing state H2G")
         self.dosth_ser()
         return 'success'
 
-class GoToPosition(smach.State):
+class G2H(smach.State):
     def __init__(self, outcomes=['success', 'fail']):
         super().__init__(outcomes)
         self.gotopos = rospy.ServiceProxy('robot/go_to_position', GoToPosition)
     
     def execute(self, ud):
-        rospy.loginfo("Executing state GoToPosition")
+        rospy.loginfo("Executing state G2H)
         req = GoToPositionRequest()
         req.position_name = 'home'
         self.gotopos(req)
@@ -33,12 +38,14 @@ class RobotState(object):
         sm = smach.StateMachine(outcomes=['---finish---'])
 
         with sm:
-            smach.StateMachine.add('DoSth1', DoSth(), 
-                               transitions={'success':'Gotoposition', 'fail':'DoSth1'})
-            smach.StateMachine.add('Gotoposition', GoToPosition(), 
-                                transitions={'success':'DoSth2', 'fail':'Gotoposition'})
+            smach.StateMachine.add('IDLE', IDLE(), 
+                                transitions={'success': 'start', 'fail': 'IDLE'})
+            smach.StateMachine.add('H2G', H2G(), 
+                                transitions={'success':'G2H', 'fail':'H2G'})
+            smach.StateMachine.add('G2H', G2H(), 
+                                transitions={'success':'---finish---', 'fail':'G2H'})
             smach.StateMachine.add('DoSth2', DoSth(), 
-                               transitions={'success':'---finish---', 'fail':'DoSth2'})
+                                transitions={'success':'---finish---', 'fail':'DoSth2'})
         outcome = sm.execute()
 
 if __name__ == "__main__":
