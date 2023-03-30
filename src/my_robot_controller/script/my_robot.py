a79f1b0cf860399df8774f6682c9c38d9f261331
PasuKriangmongkol <dominicsz2012@gmail.com>
2023-03-30 20:23:34 +0700

    changed

--- src/my_robot_controller/script/my_robot.py
+++ src/my_robot_controller/script/my_robot.py
@@ -3,99 +3,98 @@ import rospy
 import smach
 import smach_ros
 from std_srvs.srv import *
-from your_smach.srv import GoToPosition, GoToPositionRequest
 import pyttsx3
 import speech_recognition as sr
 
-class IDLE(smach.state) :
-    def __init__(self) -> None:
-        super().__init__(self,outcomes=['success', 'fail'])
+class IDE(smach.State):
+    def __init__(self):
+        super().__init__(outcomes=['success', 'fail'])
 
     def execute(self, ud):
+        print("Starting robot state")
         rospy.loginfo("Starting robot state")
-        S = str(input("Press S to start: "))
-        if S == "S":
+        S = str(input("Press s to start: "))
+        if S == "s":
             return 'success'
         else:
             return 'fail'
 
-
-
 class GoToGuest(smach.State):
-    def __init__(self, outcomes=['success', 'fail']):
-        super().__init__(outcomes)
-        
+    def __init__(self):
+        super().__init__(outcomes=['success', 'fail'])
+
     def execute(self, ud):
         rospy.loginfo("Executing state GoToGuest")
         # Navigation to GUEST code #
         #------------------------
 
         engine = pyttsx3.init()
-        engine.say("What would you like to drink")
+        engine.say("What would you like to drink?")
         engine.runAndWait()
 
         r = sr.Recognizer()
-        while(1):
-
+        while True:
             try:
                 with sr.Microphone() as source2:
                     print(">>>>>>>>>>>>>> Initializing .....")
                     r.adjust_for_ambient_noise(source2, duration=0.2)
-            
+
                     print(">>>>>>>>>>>>>> Listing .....")
 
                     # listens for the user's input
                     audio2 = r.listen(source2)
 
-                    # Using google to recognize audio
+                    # Using Google to recognize audio
                     MyText = r.recognize_google(audio2)
                     MyText = MyText.lower()
 
-                    print(MyText) 
+                    print(MyText)
 
-                    Mytext = str(MyText)
-
-                    return 'success' , MyText
-                    
+                    return 'success', MyText
 
             except sr.RequestError as e:
                 print("Could not request results; {0}".format(e))
                 return 'fail'
 
             except sr.UnknownValueError:
-                print("unknown error occurred")
+                print("Unknown error occurred")
                 return 'fail'
 
-
-
 class GoToHost(smach.State):
-    def __init__(self, outcomes=['success', 'fail']):
-        super().__init__(outcomes)
-    
-    def execute(self, ud,MyText):
+    def __init__(self):
+        super().__init__(outcomes=['success', 'fail'])
+
+    def execute(self, ud):
         rospy.loginfo("Executing state GoToHost")
-        # Navigation to GUEST code #
-        #---------------------------
-        
-        RobotText = "He want to drink" + MyText
+        # Navigation to HOST code #
+        #-------------------------
+
+        MyText = ud.GoToGuest_Output
+        RobotText = "He wants to drink " + MyText
+        print(RobotText)
+
         engine = pyttsx3.init()
-        engine.say("RobotText")
+        engine.say(RobotText)
         engine.runAndWait()
-        return 'success' 
+
+        return 'success'
 
 class RobotState(object):
-    def __init__(self) -> None:
+    def __init__(self):
         rospy.init_node('robot_state', anonymous=True)
         sm = smach.StateMachine(outcomes=['---finish---'])
 
         with sm:
-            smach.StateMachine.add('IDLE', IDLE(), 
-                                transitions={'success': 'GoToGuest', 'fail': 'IDLE'})
-            smach.StateMachine.add('GoToGuest', GoToGuest(), 
-                                transitions={'success':'GoToHost', 'fail':'GoToGuest'})
-            smach.StateMachine.add('GoToHost', GoToHost(), 
-                                transitions={'success':'---finish---', 'fail':False})
+            smach.StateMachine.add('IDE', IDE(),
+                                transitions={'success': 'GoToGuest', 'fail': 'IDE'})
+            smach.StateMachine.add('GoToGuest', GoToGuest(),
+                                transitions={'success': 'GoToHost', 'fail': 'GoToGuest'},
+                                remapping={'GoToGuest_Output': 'MyText'})
+            smach.StateMachine.add('GoToHost', GoToHost(),
+                                transitions={'success': '---finish---', 'fail': False})
+
         outcome = sm.execute()
 
-if __name__ == "__main__":
+if __name__ == "_main_":
+    print("Starting robot")
     RobotState()
